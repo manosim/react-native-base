b2f88310392e415dad48f4839e240dc929c321fb
Emmanouil Konstantinidis <hello@manos.im>
2016-11-12 20:48:08 +0000

    Improve install script & show some info prior to installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,44 +4,69 @@ set -e
 reset="\e[0m"
 red="\e[0;31m"
 green="\e[0;32m"
+yellow="\e[0;33m"
 cyan="\e[0;36m"
 white="\e[0;37m"
 
+BRANCH_NAME="setup-app"
 ZIP_FILENAME="react-native-base.zip"
-ZIP_DESTINATION="react-native-base-setup-app"
+ZIP_DESTINATION="react-native-base-$BRANCH_NAME"
 APP_DESTINATION="App"
 
+info() {
+    printf "\n"
+    printf "$cyan  React Native Base$reset\n"
+    printf "$cyan  =================$reset\n"
+    printf "$cyan  https://www.github.com/manosim/react-native-base$reset\n\n"
+
+    printf "$yellow> Is this a fresh project? If not we suggest commiting any pending changes first.$reset\n"
+    printf "$yellow> React Native Base will overwrite the project's index.ios.js & index.android.js.$reset\n"
+    printf "$yellow> If you are not sure, please visit the GitHub repository.$reset\n\n"
+}
+
+are_you_sure() {
+    printf "$cyan> Are you sure you want to continue? "
+    read -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    else
+        printf "\n"
+    fi
+}
+
 download_zip() {
-    printf "$green> Downloading zip.$reset\n"
-    curl -s "https://codeload.github.com/manosim/react-native-base/zip/setup-app" --output $ZIP_FILENAME
+    printf "$cyan> Downloading zip.$reset\n"
+    curl -s "https://codeload.github.com/manosim/react-native-base/zip/$BRANCH_NAME" --output $ZIP_FILENAME
 }
 
 unzip_app() {
-    printf "$green> Unzipping.$reset\n"
+    printf "$cyan> Unzipping.$reset\n"
     unzip -qo $ZIP_FILENAME
 }
 
 copy_files() {
-    printf "$green> Copying files.$reset\n"
+    printf "$cyan> Copying files.$reset\n"
     mkdir -p $APP_DESTINATION
     cp "$ZIP_DESTINATION/.babelrc" .
     cp -r "$ZIP_DESTINATION/$APP_DESTINATION/" $APP_DESTINATION
 }
 
 clean_up() {
-    printf "$green> Cleaning Up.$reset\n"
+    printf "$cyan> Cleaning Up.$reset\n"
     rm -f $ZIP_FILENAME
     rm -rf $ZIP_DESTINATION
 }
 
 install() {
-    printf "$green> Installing react-native-base.$reset\n"
+    info
+    are_you_sure
+
+    printf "\n$cyan> Installing react-native-base.$reset\n"
     download_zip
     unzip_app
     copy_files
     clean_up
 }
 
-
-printf "$green> Setting Up react-native-base.$reset\n"
 install
