dd71e135ff7241c6ae881b8bde09e8c1a44f686d
Emmanouil Konstantinidis <hello@manos.im>
2016-11-12 20:11:52 +0000

    Unzip quietly & copy files

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,8 @@ cyan="\e[0;36m"
 white="\e[0;37m"
 
 ZIP_FILENAME="react-native-base.zip"
-ZIP_DESTINATION="react-native-base-setup-app/"
+ZIP_DESTINATION="react-native-base-setup-app"
+APP_DESTINATION="App"
 
 download_zip() {
     printf "$green> Downloading zip.$reset\n"
@@ -17,19 +18,20 @@ download_zip() {
 
 unzip_app() {
     printf "$green> Unzipping.$reset\n"
-    unzip -o -q $ZIP_FILENAME
+    unzip -qo $ZIP_FILENAME
 }
 
 copy_files() {
     printf "$green> Copying files.$reset\n"
-
+    mkdir -p $APP_DESTINATION
+    cp "$ZIP_DESTINATION/.babelrc" .
+    cp -r "$ZIP_DESTINATION/$APP_DESTINATION/" $APP_DESTINATION
 }
 
 clean_up() {
     printf "$green> Cleaning Up.$reset\n"
     rm -f $ZIP_FILENAME
     rm -rf $ZIP_DESTINATION
-
 }
 
 install() {
