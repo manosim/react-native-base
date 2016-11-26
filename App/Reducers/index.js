82ea1a5a9ccb8028983039aa635cb0a2e28fc7e5
Emmanouil Konstantinidis <hello@manos.im>
2016-11-26 00:05:10 +0000

    Remove example reducer

--- App/Reducers/index.js
+++ App/Reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
-import exampleReducer from './ExampleReducer';
 
 export default combineReducers({
-  exampleReducer
+  exampleReducer: (state = {}, action) => state
 });
