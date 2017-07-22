69a380b27596d78b5d2e3a7e084cbcb45e624825
Emmanouil Konstantinidis <hello@manos.im>
2017-07-22 17:32:40 +0100

    Fix navigator import

--- app/app-container.js
+++ app/app-container.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Navigator } from 'react-native-deprecated-custom-components';
 
 import {
   BackAndroid,
   StyleSheet,
-  Navigator,
   View,
 } from 'react-native';
 
